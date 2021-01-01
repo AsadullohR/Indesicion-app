31392956e085d4c7e555a94507f9ea108d92b7ff
AsadullohR <58390694+AsadullohR@users.noreply.github.com>
2021-01-01 16:12:33 +0100

    setup node_modules and babel

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+
+const template = <h1>Indesicion app.</h1>;
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
